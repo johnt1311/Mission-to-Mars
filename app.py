325515ef88d7296ea842369ec7e2a041bd35b872
John Temple <JohnTemple@safeguardworld.com>
2021-05-19 10:55:35 -0500

    challenge

---
+++ app.py
@@ -0,0 +1,28 @@
+# initiate flask
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# homepage
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+#scrape button
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
+
+# run flask
+if __name__ == "__main__":
+   app.run()
